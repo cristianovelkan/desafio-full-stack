9e8575762aa3dc79d2ebff7a724f2adae5012c97
Cristiano <cristianovelkan@gmail.com>
2024-11-11 10:09:42 -0300

    feat: add Uuid to User

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,38 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Traits\HasUuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, HasApiTokens;
+    use HasFactory, Notifiable, HasApiTokens, HasUuid;
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The primary key's datatype.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * The ID field is a UUID
+     *
+     * @var bool
+     */
+    protected bool $primaryKeyIsUuid = true;
 
     /**
      * The attributes that are mass assignable.
@@ -43,6 +67,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'id' => 'string',
         ];
     }
 }
